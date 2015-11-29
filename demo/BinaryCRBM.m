03cbc42b31e01a47bdb0dd7ea3c1837b9e320af4
Atul Jha <atul1805@gmail.com>
2015-11-28 21:55:52 -0500

    Updated code

--- demo/BinaryCRBM.m
+++ demo/BinaryCRBM.m
@@ -1,100 +1,123 @@
-%fprintf('\nHere we train a Convolutional RBM on the MNIST Dataset.\n');
 %dataset = 'mnistSmall';
 
 %[images, tags] load('data.mat');
-testRun = 2;
-[images, tags, m, n] = loadImagesTags;
-dataSize = [m,n,1];  % [nY x nX x nChannels]
+numTestRun = 1;
+finalResult = cell(numTestRun);
+for testRun = 1 : numTestRun;
+    [images, tags, m, n] = loadImagesTags;
+    disp('Data Read');
+    dataSize = [m,n,1];  % [nY x nX x nChannels]
 
-trainTag = cell(1,1);
-testTag = cell(1,1);
-experiments = size(images);
-trainImgCount = 1;
-testImgCount = 1;
-for i = 1 : experiments(1)
-    experimentSize = size(images{i,1});
-    if i ~= testRun
-        for j = 1 : experimentSize(1)
-            trainData(:,:,trainImgCount) = images{i,1}{j,2};
-            trainTag{trainImgCount,1} = images{i,1}{j,3};
-            trainImgCount = trainImgCount + 1;
-        end
-    else
-        for j = 1 : experimentSize(1)
-            testData(:,:,testImgCount) = images{i,1}{j,2};
-            testTag{testImgCount,1} = images{i,1}{j,3};
-            testImgCount = testImgCount + 1;
-        end
-    end
-end
-
-% DEFINE AN ARCHITECTURE
-arch = struct('dataSize', dataSize, ...
-		'nFM', 9, ...
-        'filterSize', [7 7], ...
-        'stride', [2 2], ...
-        'inputType', 'binary');
-
-% GLOBAL OPTIONS
-arch.opts = {'nEpoch', 1, ...
-			 'lRate', .00001, ...
-			 'displayEvery',100, ...
-			 'sparsity', .02, ...
-			 'sparseGain', 5};% , ...
-%  			 'visFun', @visBinaryCRBMLearning}; % UNCOMMENT TO VIEW LEARNING
-
-% INITIALIZE AND TRAIN
-cr = crbm(arch);
-cr = cr.train(trainData);
-
-for i = 1 : (trainImgCount - 1)
-    % INFER HIDDEN AND POOLING LAYER EXPECTATIONS
-    % CONDITIONED ON SOME INPUT
-    [cr,ep] = cr.poolGivVis(trainData(:,:,i));
-    cr = cr.hidGivVis(trainData(:,:,i));
-
-    [nCols,nRows,k]=size(cr.eHid);
-    eh = reshape(cr.eHid,nRows*nCols*k,1);
-    trainFeatures(i,:) = eh;
-end
-
-% INITIALIZE AND TRAIN
-cr = crbm(arch);
-cr = cr.train(testData);
-
-for i = 1 : (testImgCount - 1)
-    % INFER HIDDEN AND POOLING LAYER EXPECTATIONS
-    % CONDITIONED ON SOME INPUT
-    [cr,ep] = cr.poolGivVis(testData(:,:,i));
-    cr = cr.hidGivVis(testData(:,:,i));
-
-    [nCols,nRows,k]=size(cr.eHid);
-    eh = reshape(cr.eHid,nRows*nCols*k,1);
-    testFeatures(i,:) = eh;
-end
-
-models = cell(length(tags),1);
-% SVM Training
-for i = 1 : length(tags)
-    trainLabel = ones((trainImgCount - 1), 1)*-1;
-    for j = 1 : (trainImgCount - 1)
-        if ismember(tags{i,1}, trainTag{j,1})
-            trainLabel(j,1) = 1;
+    trainTag = cell(1,1);
+    testTag = cell(1,1);
+    experiments = size(images);
+    trainImgCount = 1;
+    testImgCount = 1;
+    for i = 1 : experiments(1)
+        experimentSize = size(images{i,1});
+        if i ~= testRun
+            for j = 1 : experimentSize(1)
+                trainData(:,:,trainImgCount) = images{i,1}{j,2};
+                trainTag{trainImgCount,1} = images{i,1}{j,3};
+                trainImgCount = trainImgCount + 1;
+            end
+        else
+            for j = 1 : experimentSize(1)
+                testData(:,:,testImgCount) = images{i,1}{j,2};
+                testTag{testImgCount,1} = images{i,1}{j,3};
+                testImgCount = testImgCount + 1;
+            end
         end
     end
-    models{i,1} = svmtrain(trainLabel,trainFeatures);
-end
+    disp('Training and Test data created');
+    clear images;
 
-% Predict
-result = cell(length(tags),2);
-for i = 1 : length(tags)
-    testLabel = ones((testImgCount - 1), 1)*-1;
-    for j = 1 : (testImgCount - 1)
-        if ismember(tags{i,1}, testTag{j,1})
-            testLabel(j,1) = 1;
-        end
+    % DEFINE AN ARCHITECTURE
+    arch = struct('dataSize', dataSize, ...
+        	'nFM', 3, ...
+            'filterSize', [7 7], ...
+            'stride', [2 2], ...
+            'inputType', 'binary');
+
+    % GLOBAL OPTIONS
+    arch.opts = {'nEpoch', 1, ...
+        		 'lRate', .00001, ...
+            	 'displayEvery',1, ...
+                 'sparsity', .02, ...
+                'sparseGain', 5};% , ...
+%               'visFun', @visBinaryCRBMLearning}; % UNCOMMENT TO VIEW LEARNING
+
+    % INITIALIZE AND TRAIN
+    cr = crbm(arch);
+    cr = cr.train(trainData);
+    disp('CRBM Training done for trainData');
+
+    i = 1;
+    while ~isempty(trainData)
+        % INFER HIDDEN AND POOLING LAYER EXPECTATIONS
+        % CONDITIONED ON SOME INPUT
+        [cr,ep] = cr.poolGivVis(trainData(:,:,1));
+        cr = cr.hidGivVis(trainData(:,:,1));
+        [nCols,nRows,k]=size(cr.eHid);
+        trainFeatures(i,:) = reshape(cr.eHid,nRows*nCols*k,1);
+        i = i + 1;
+        trainData(:,:,1) = [];
     end
-    [predict_label, accuracy, prob_estimates] = svmpredict(testLabel,testFeatures,models{i,1},'-b 1');
-    result{i,1} = [predict_label, prob_estimates, testLabel, tags{i,1}*ones((testImgCount - 1), 1)];
-    result{i,2} = accuracy;
+    clear trainData;
+    clear cr;
+    disp('Train features extracted');
+
+    % INITIALIZE AND TRAIN
+    cr = crbm(arch);
+    cr = cr.train(testData);
+    disp('CRBM Training done for testData');
+    
+    i = 1;
+    while ~isempty(testData)
+        % INFER HIDDEN AND POOLING LAYER EXPECTATIONS
+        % CONDITIONED ON SOME INPUT
+        [cr,ep] = cr.poolGivVis(testData(:,:,1));
+        cr = cr.hidGivVis(testData(:,:,1));
+        [nCols,nRows,k]=size(cr.eHid);
+        testFeatures(i,:) = reshape(cr.eHid,nRows*nCols*k,1);
+        i = i + 1;
+        testData(:,:,1) = [];
+    end
+    clear testData;
+    clear cr;
+    disp('Test features extracted');
+
+    models = cell(length(tags),1);
+    % SVM Training
+    for i = 1 : length(tags)
+        trainLabel = ones((trainImgCount - 1), 1)*-1;
+        for j = 1 : (trainImgCount - 1)
+            if ismember(tags{i,1}, trainTag{j,1})
+                trainLabel(j,1) = 1;
+            end
+        end
+        models{i,1} = svmtrain(double(trainLabel),double(trainFeatures));
+    end
+    clear trainFeatures;
+    clear trainLabel;
+
+    disp('SVM traininig done');
+    % Predict
+    %result = cell(length(tags),2);
+    for i = 1 : length(tags)
+        testLabel = ones((testImgCount - 1), 1)*-1;
+        for j = 1 : (testImgCount - 1)
+            if ismember(tags{i,1}, testTag{j,1})
+                testLabel(j,1) = 1;
+            end
+        end
+        [predict_label, accuracy, dec_values] = svmpredict(double(testLabel),double(testFeatures),models{i,1});
+        finalResult{testRun,i,1} = [predict_label, testLabel, tags{i,1}*ones((testImgCount - 1), 1)];
+        finalResult{testRun,i,2} = accuracy;
+    end
+    disp('Prediction done');
+    
+    clear testFeatures;
+    clear testLabel;
+    clear models;
 end
--- loadImagesTags.m
+++ loadImagesTags.m
@@ -2,32 +2,37 @@ function [ images,tags,m,n ] = loadImagesTags()
 
     images = cell(2,1);
     tags = cell(1,2);
-    imageDir = 'C:\Users\Atul\Documents\MATLAB\CV\xray_scattering\sample_image\';
+    %imageDir = 'C:\Users\Atul\Documents\MATLAB\CV\xray_scattering\sample_image\';
+    imageDir = '/media/atul/OS/Users/Atul/Documents/MATLAB/CV/xray_scattering/sample_image';
     Files=dir(imageDir);
     i = 1;
+    j = 1;
     for k=3:length(Files)
         FileName=Files(k).name;
-        image =  im2double(imread(strcat(imageDir,FileName)));
+        image =  im2double(imread([imageDir, '/', FileName]));
         [m,n] = size(image);
         image = imresize(image,1024/m);
         image = (image - mean(image(:)));% / std(image(:));
         [m,n] = size(image);
-        if k ~= length(Files)
+        if k > 0.75*length(Files)
             images{1}{i,1} = FileName;
-            images{1}{i,2} = image;%reshape(image,m,n,1);
+            images{1}{i,2} = image;
             i = i + 1;
         else
-            images{2}{1,1} = FileName;
-            images{2}{1,2} = reshape(image,m,n,1);
+            images{2}{j,1} = FileName;
+            images{2}{j,2} = reshape(image,m,n,1);
+            j = j + 1;
         end
     end
-    tagDir = 'C:\Users\Atul\Documents\MATLAB\CV\xray_scattering\sample_tag';
+    %tagDir = 'C:\Users\Atul\Documents\MATLAB\CV\xray_scattering\sample_tag';
+    tagDir = '/media/atul/OS/Users/Atul/Documents/MATLAB/CV/xray_scattering/sample_tag';
     Files=dir(tagDir);
     i = 1;
+    p = 1;
     tagCount = 1;
     for k=3:length(Files)
         FileName=Files(k).name;
-        tagContent = fileread([tagDir, '\', FileName]);
+        tagContent = fileread([tagDir, '/', FileName]);
         tagIdStart = strfind(tagContent, '<tag id="');
         tagIdEnd = strfind(tagContent, '</tag>');
         tagArray = zeros(1,length(tagIdStart));
@@ -64,11 +69,12 @@ function [ images,tags,m,n ] = loadImagesTags()
             tagArray(end + 1) = 206;
         end
         
-        if k ~= length(Files)
+        if k > 0.75*length(Files)
             images{1}{i,3} = tagArray;
             i = i + 1;
         else
-            images{2}{1,3} = tagArray;
+            images{2}{p,3} = tagArray;
+            p = p + 1;
         end
     end
 end
